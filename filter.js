cbcca8896cbd28ef00e3f1a6b0ec4345bfb42ca6
unknown <Rakesh Bitling>
2021-03-30 21:42:09 +0530

    fixing stuff

--- filter.js
+++ filter.js
@@ -1,9 +1,11 @@
 const items = [
     {name: "Rakesh", price: 1},
-    {name: "Sonar", price: 23},
+    {name: "Rakesh", price: 23},
     {name: "Mahipal", price: 54}
 ];
-const filteredItems = items.find((t) =>  t.price > 23);
+const filteredItems = items.filter((t) =>  t.name == 'Rakesh').map((data)=>{
+    return data.price;
+});
 console.log(items);
 console.log("=======================");
 console.log(filteredItems)
